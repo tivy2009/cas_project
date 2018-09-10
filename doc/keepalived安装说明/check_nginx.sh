bf039119780ddab116e8981f76451ad867f3ed0d
tivy <tivy2009@126.com>
2018-09-10 16:35:53 +0800

    目录结构调整

--- doc/keepalived安装说明/check_nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ "$(ps -ef | grep "nginx: master process"| grep -v grep )" == "" ]
- then
-    killall keepalived
- else
-    echo "nginx is running"
-fi
--- doc/keepalived安装说明/keepalived_start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sleep 30s
-service keepalived start
